2679b93cb3489016ef1346a98e9ae8ac5269a55f
Harkirat Saluja <saluja.harkirat@gmail.com>
2020-02-23 18:38:40 +0530

    1. Added code for engine and rules 2. Added base code for condition

---
+++ src/engine/Condition.js
@@ -0,0 +1,10 @@
+// import Fact from "./Fact";
+
+class Condition {
+  constructor (condition = {}) {
+    this.id = condition.id;
+    this.name = condition.name;
+  }
+}
+
+export default Condition;
---
+++ src/engine/index.js
@@ -0,0 +1,5 @@
+import Engine from "./Engine";
+
+export default function (rules = []) {
+  return new Engine(rules);
+}
